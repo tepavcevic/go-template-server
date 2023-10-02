fc28e2eda04912ae25c6658eeb0880b33982e0f6
tepavcevic <djordje.tepa@gmail.com>
2023-10-02 19:11:29 +0200

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tepavcevic/go-template-server
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting server at port 8080")
+	http.ListenAndServe(":8080", nil)
+}
