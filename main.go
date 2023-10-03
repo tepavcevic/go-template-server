13998cce1fb0cb1b13e453fdf98288a66471f275
tepavcevic <djordje.tepa@gmail.com>
2023-10-03 18:58:27 +0200

    Add chi router and refactor routes to use it

--- main.go
+++ main.go
@@ -3,15 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func handlerContact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To contact me send an email to <a href=\"mailto:djordje.tepa@gmail.com\">djordje.tepa@gmail.com</a>.</p>")
+}
+
+func handlerFAQ(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(
+		w,
+		`<h1>FAQ</h1>
+		<p>Q: Is there a free version?</p>
+		<p>A: Yes! We offer a free trial for 30 days on any period plans.</p>
+		<br>
+		<p>Q: What are Your support hours?</p>
+		<p>A: Yes! We offer a free trial for 30 days on any period plans.</p>
+		<br>
+		<p>Q: Is there a free version?</p>
+		<p>A: Yes! We offer a free trial for 30 days on any period plans.</p>
+		`,
+	)
+}
+
+type Router struct{}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+	r.Get("/", handlerHome)
+	r.Get("/contact", handlerContact)
+	r.Get("/faq", handlerFAQ)
+
 	fmt.Println("Starting server at port 8080")
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 }
