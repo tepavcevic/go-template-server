804a325278f435bd408f5789aa1abfb021fe725d
tepavcevic <djordje.tepa@gmail.com>
2023-10-05 14:31:29 +0200

    Sign up page template and styling

--- main.go
+++ main.go
@@ -34,6 +34,12 @@ func main() {
 	))
 	r.Get("/faq", controllers.FAQ(tpl))
 
+	tpl = views.Must(views.ParseFS(
+		templates.FS,
+		"signup.gohtml", "tailwind.gohtml",
+	))
+	r.Get("/signup", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
