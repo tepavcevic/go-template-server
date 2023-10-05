44ef9eeb0107a9df35b8f15106dd7b18000969b4
tepavcevic <djordje.tepa@gmail.com>
2023-10-05 10:38:51 +0200

    FAQ page dynamic data, removed boilerplate html

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/tepavcevic/go-template-server/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version",
+			Answer:   "Yes! We offer a free trial for 30 days on any period plans.",
+		}, {
+			Question: "What are Your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response time may be slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@example.com">support@example.com</a>.`,
+		},
+		{
+			Question: "Where is your office?",
+			Answer:   "Our support team is based in Sekovici, BiH.",
+		},
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
