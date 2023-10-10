2f4b11c05512cb08bde7f88dc3a6e792dc27768a
tepavcevic <djordje.tepa@gmail.com>
2023-10-10 19:56:27 +0200

    Add rand pckg for generating session tokens

---
+++ rand/rand.go
@@ -0,0 +1,35 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	nRead, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("bytes: %w", err)
+	}
+	if nRead < n {
+		return nil, fmt.Errorf("bytes: didn't read enough random bytes")
+	}
+	return b, nil
+}
+
+// String takes the nubmer of bytes being used to generate a random string
+// of same length using base64 URL encoding
+func String(n int) (string, error) {
+	b, err := Bytes(n)
+	if err != nil {
+		return "", fmt.Errorf("string: %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+const SessionTokenBytes = 32
+
+func SessionToken() (string, error) {
+	return String(SessionTokenBytes)
+}
